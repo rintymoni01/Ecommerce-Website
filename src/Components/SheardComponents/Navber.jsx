fa28e9f6419f4979991d77bf71fd57326075a43c
rintymoni01 <rintymoni01@gmail.com>
2026-09-13 19:36:02 +0600

    isActive add

--- src/Components/SheardComponents/Navber.jsx
+++ src/Components/SheardComponents/Navber.jsx
@@ -2,33 +2,44 @@ import { Link, NavLink } from "react-router-dom";
 import { LuMapPin } from "react-icons/lu";
 import { FaCaretDown } from "react-icons/fa";
 const Navber = () => {
-  const location =false
+  const location = false;
   return (
     <div className="bg-white py-3 shadow-2xl">
-     
       <div className="max-w-6xl mx-auto flex justify-between items-center">
-       <div className="flex gap-7 items-center">
-         <Link to={"/"} className="text-3xl font-bold">
-               {/* logo */}
-        <span className="text-red-500 font-serif">R</span>inty
-      </Link>
-      <div className="text-gray-700 flex items-center gap-1 cursor-pointer">
-        <LuMapPin className="text-red-500"/>
-        <span className="font-semibold">{location ? <div></div> : "Add Address"}</span>
-        <FaCaretDown />
-       </div>
-      </div>
-      <nav>
-        <ul className="flex gap-7 items-center font-semibold">
-          <NavLink to={"/"}><li>Home</li></NavLink>
-          <NavLink to={"/products"}><li>Products</li></NavLink>
-          <NavLink to={"/about"}><li>About</li></NavLink>
-          <NavLink to={"/contact"}><li>Contact</li></NavLink>
-         
-        
-         
-        </ul>
-      </nav>
+        <div className="flex gap-7 items-center">
+          <Link to={"/"} className="text-3xl font-bold">
+            {/* logo */}
+            <span className="text-red-500 font-serif">R</span>inty
+          </Link>
+          <div className="text-gray-700 flex items-center gap-1 cursor-pointer">
+            <LuMapPin className="text-red-500" />
+            <span className="font-semibold">
+              {location ? <div></div> : "Add Address"}
+            </span>
+            <FaCaretDown />
+          </div>
+        </div>
+        <nav>
+          <ul className="flex gap-7 items-center font-semibold">
+            <NavLink
+              to={"/"}
+              className={({isActive}) =>
+                `${isActive ? "border-b-3 transition-all border-red-500"}`
+              }
+            >
+              <li>Home</li>
+            </NavLink>
+            <NavLink to={"/products"}>
+              <li>Products</li>
+            </NavLink>
+            <NavLink to={"/about"}>
+              <li>About</li>
+            </NavLink>
+            <NavLink to={"/contact"}>
+              <li>Contact</li>
+            </NavLink>
+          </ul>
+        </nav>
       </div>
     </div>
   );
