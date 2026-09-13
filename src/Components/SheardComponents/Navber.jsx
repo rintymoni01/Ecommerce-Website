ae210e08423c980248ce5265763e97f87138706a
rintymoni01 <rintymoni01@gmail.com>
2026-09-13 19:11:07 +0600

    location add

--- src/Components/SheardComponents/Navber.jsx
+++ src/Components/SheardComponents/Navber.jsx
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
 import { LuMapPin } from "react-icons/lu";
+import { FaCaretDown } from "react-icons/fa";
 const Navber = () => {
+  const location =false
   return (
     <div className="bg-white py-3 shadow-2xl">
      
       <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <Link to={"/"} className="text-3xl font-bold">
+       <div className="flex gap-7 items-center">
+         <Link to={"/"} className="text-3xl font-bold">
                {/* logo */}
         <span className="text-red-500 font-serif">R</span>inty
       </Link>
       <div className="text-gray-700 flex items-center gap-1 cursor-pointer">
-        <LuMapPin />
+        <LuMapPin className="text-red-500"/>
+        <span className="font-semibold">{location ? <div></div> : "Add Address"}</span>
+        <FaCaretDown />
+       </div>
       </div>
       </div>
     </div>
