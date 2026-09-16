f6d2e899f8b828f5d0eef400a644c382163af76f
rintymoni01 <rintymoni01@gmail.com>
2026-09-16 21:49:13 +0600

    item.discription add

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -28,23 +28,30 @@ const Carousel = () => {
   return (
     <div>
       <Slider {...settings}>
-        {
-            data?.slice(0,7)?.map((item, index)=>{
-                return <div key={index} className="bg-gradient-to-r from-[#0f0c29] via-[#302b63] to-[#24243e] -z-10">
-<div className=" flex gap-10 justify-center h-[600px] items-center px-4">
-    <div className="space-y-6">
-        <h3 className="text-red-500 font-semibold font-sans text-sm">Powering your world with tha best in Electronics</h3>
-<h1>{item.title}</h1>
-    </div>
-
-</div>
+        {data?.slice(0, 7)?.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="bg-gradient-to-r from-[#0f0c29] via-[#302b63] to-[#24243e] -z-10"
+            >
+              <div className=" flex gap-10 justify-center h-[600px] items-center px-4">
+                <div className="space-y-6">
+                  <h3 className="text-red-500 font-semibold font-sans text-sm">
+                    Powering your world with tha best in Electronics
+                  </h3>
+                  <h1 className="text-4xl font-bold text-white uppercase md:w-[500px]">
+                    {item.title}
+                  </h1>
+                  <h4 className="md:w-[500px] line-clamp-3 text-gray-400 pr-7">{item.description}</h4>
+                  <button></button>
                 </div>
-
-            })
-        }
-      <div>
-        <h1>1</h1>
-      </div>
+              </div>
+            </div>
+          );
+        })}
+        <div>
+          <h1>1</h1>
+        </div>
       </Slider>
     </div>
   );
