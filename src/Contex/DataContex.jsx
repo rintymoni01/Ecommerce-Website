12adbe2a5756a1cd2d62bc418565f13ac4a0bee5
rintymoni01 <rintymoni01@gmail.com>
2026-09-16 20:13:35 +0600

    fakeApi add

--- src/Contex/DataContex.jsx
+++ src/Contex/DataContex.jsx
@@ -1,6 +1,20 @@
-import { createContext } from "react";
+import axios from "axios";
+import { createContext, useState } from "react";
 
-export const DataContex = createContext (null)
-export const DataProvider =()=>{
-    const {data, setData}
-}
+export const DataContex = createContext(null);
+export const DataProvider = ({ children }) => {
+  const { data, setData } = useState();
+  // fatching all products from api
+  const fatchAllProducts = async () => {
+    try {
+       const res = await axios.get("https://dummyjson.com/products?limit=150") 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <DataContex.Provider value={{ data, setData }}>
+      {children}
+    </DataContex.Provider>
+  );
+};
