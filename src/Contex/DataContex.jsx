68eeed8204ef93453c8799a9f5032d89b69375e1
rintymoni01 <rintymoni01@gmail.com>
2026-09-16 20:45:50 +0600

    productsData add

--- src/Contex/DataContex.jsx
+++ src/Contex/DataContex.jsx
@@ -12,10 +12,10 @@ export const DataProvider = ({ children }) => {
       const res = await axios.get(
         "https://dummyjson.com/products?limit=150"
       );
+      console.log(res);
+      const productsData = res.data.products 
+      setData (productsData)
 
-      setData(res.data.products);
-
-      console.log(res.data.products);
     } catch (error) {
       console.log(error);
     }
