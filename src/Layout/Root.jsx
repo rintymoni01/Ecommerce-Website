c94555ed940afe2a6b85310626fb7ffd79046cf7
rintymoni01 <rintymoni01@gmail.com>
2026-09-16 14:06:38 +0600

    await

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,6 +1,6 @@
 
 
-import { Outlet } from "react-router-dom";
+import { Await, Outlet } from "react-router-dom";
 import Navber from "../Components/SheardComponents/Navber";
 import { useState } from "react";
 
@@ -12,6 +12,10 @@ const Root = () => {
             const{latiyude,longitude}=pos.coords 
             console.log(TbWorldLatitude,longitude);
             const url="https.//nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json"
+            try{
+                const location=Await axios.get(url)
+                const 
+            }
             
         })
     }
