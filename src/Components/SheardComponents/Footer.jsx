45efeda01c156a166dfaae2d98d103909fb1632d
rintymoni01 <rintymoni01@gmail.com>
2026-09-17 20:00:07 +0600

    css add

--- src/Components/SheardComponents/Footer.jsx
+++ src/Components/SheardComponents/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
             <p className="text-red-500 text-3xl font-serif ">Rinty</p>
 <div>
     <p className="text-sm text-gray-400">Enhance Your Beauty with Premium Cosmetics.</p>
-<p className="text-sm text-gray-400">123 Beauty Street, New York, NY 10001</p>
-<p className="text-sm text-gray-400">Email: support@glowea.com</p>
-<p className="text-sm text-gray-400">Phone: (123) 456-7890</p>
+<p className="text-xs text-gray-400">123 Beauty Street, New York, NY 10001</p>
+<p className="text-xs text-gray-400">Email: support@glowea.com</p>
+<p className="text-xs text-gray-400">Phone: (123) 456-7890</p>
 </div>
 
 
