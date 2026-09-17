bd86243320c1eb2dba66c58644a55578e870f18b
rintymoni01 <rintymoni01@gmail.com>
2026-09-17 19:04:27 +0600

    map

--- src/Pages/Home/HomeComponents/Fetures.jsx
+++ src/Pages/Home/HomeComponents/Fetures.jsx
@@ -9,7 +9,15 @@ const fetures =[
 const Fetures = () => {
     return (
         <div className="bg-gray-100  py-8 px-4 sm:px-6  lg:px-8">
-            <div className="">
+            <div className="max:w-7xl mx-auto">
+                <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-2 lg:grid-cols-4 lg:gap-x-8">
+                    {
+                        fetures.map((fetures, index)=>{
+
+                        })
+                    }
+
+                </div>
 
             </div>
             
