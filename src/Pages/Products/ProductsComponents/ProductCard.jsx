41cc126e969424c4c9b46813a36a3c69fb8c0f8b
rintymoni01 <rintymoni01@gmail.com>
2026-09-17 22:34:38 +0600

    title or price add

--- src/Pages/Products/ProductsComponents/ProductCard.jsx
+++ src/Pages/Products/ProductsComponents/ProductCard.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-
-const ProductCard = () => {
-    return (
-        <div className='border relative border-gray-100 rounded-2xl cursor-pointer'>
-            <div>
-
-            </div>
-        </div>
-    );
+const ProductCard = ({product}) => {
+    console.log(product);
+    
+  return (
+    <div className="border relative border-gray-100 rounded-2xl hover:scale-105 hover:shadow-2xl h-max transition-all  cursor-pointer">
+      <img src={product.images} alt=""  className="bg-gray-100 aspect-square"/>
+      <h1 className="line-clamp-2 py-1 font-semibold">{product.title}</h1>
+      <p className="my-1 text-lg text-gray-800 font-bold">${product.price}</p>
+      
+    </div>
+  );
 };
 
 export default ProductCard;
