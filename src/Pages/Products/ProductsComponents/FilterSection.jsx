92efc7896768719d50c53ae6822103bbca0b861a
rintymoni01 <rintymoni01@gmail.com>
2026-09-18 19:40:18 +0600

    css add

--- src/Pages/Products/ProductsComponents/FilterSection.jsx
+++ src/Pages/Products/ProductsComponents/FilterSection.jsx
@@ -5,7 +5,10 @@ const FilterSection = () => {
         <div className="bg-gray-100  mt-10 p-4 rounded-md h-max">
            <input type="text" placeholder="Search..." className="bg-white p-2 rounded-md border-gray-400 border-2"/>
            {/* category dara */}
-           <h1></h1>
+           <h1 className=" mt-5 text-xl font-semibold">Category</h1>
+           <div className="flex flex-col mt-3 gap-2 ">
+
+           </div>
         </div>
     );
 };
