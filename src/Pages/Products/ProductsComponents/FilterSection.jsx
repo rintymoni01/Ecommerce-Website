f488a5d35092a4c67a3e34f2604dfcb3ce6aff17
rintymoni01 <rintymoni01@gmail.com>
2026-09-18 19:36:57 +0600

    category

--- src/Pages/Products/ProductsComponents/FilterSection.jsx
+++ src/Pages/Products/ProductsComponents/FilterSection.jsx
@@ -2,8 +2,10 @@
 
 const FilterSection = () => {
     return (
-        <div className="bg-gray-100  pt-10 p-4 rounded-md h-max">
-           <input type="text" placeholder="Search..."
+        <div className="bg-gray-100  mt-10 p-4 rounded-md h-max">
+           <input type="text" placeholder="Search..." className="bg-white p-2 rounded-md border-gray-400 border-2"/>
+           {/* category dara */}
+           <h1></h1>
         </div>
     );
 };
