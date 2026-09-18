7500fa18000685cb16d11ec76997539dbae272bf
rintymoni01 <rintymoni01@gmail.com>
2026-09-18 19:27:27 +0600

    button

--- src/Pages/Products/ProductsComponents/ProductCard.jsx
+++ src/Pages/Products/ProductsComponents/ProductCard.jsx
@@ -1,12 +1,18 @@
-const ProductCard = ({product}) => {
-    console.log(product);
+
+import { IoCartOutline } from "react-icons/io5"; // Sothik import path
+
+const ProductCard = ({ product }) => {
+  console.log(product);
     
   return (
-    <div className="border relative border-gray-100 rounded-2xl hover:scale-105 hover:shadow-2xl h-max transition-all  cursor-pointer">
-      <img src={product.images} alt=""  className="bg-gray-100 aspect-square"/>
+    <div className="border relative border-gray-100 rounded-2xl hover:scale-105 hover:shadow-2xl h-max transition-all cursor-pointer p-4">
+      <img src={product.images} alt="" className="bg-gray-100 aspect-square w-full object-cover rounded-lg"/>
       <h1 className="line-clamp-2 py-1 font-semibold">{product.title}</h1>
       <p className="my-1 text-lg text-gray-800 font-bold">${product.price}</p>
       
+      <button className="flex items-center text-lg cursor-pointer justify-center font-semibold bg-red-500 w-full  gap-2 text-white px-3 py-2 rounded-md mt-2">
+        <IoCartOutline className="w-6 h-6" /> Add to Cart 
+      </button>
     </div>
   );
 };
--- src/Pages/Products/ProductsComponents/ProductsComponents.jsx
+++ src/Pages/Products/ProductsComponents/ProductsComponents.jsx
@@ -18,7 +18,7 @@ const ProductsComponents = () => {
             
             <FilterSection />
 
-            <div className="flex-1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-4 mt-10 gap-7">
               {data.map((product) => (
                 <ProductCard
                   key={product.id}
