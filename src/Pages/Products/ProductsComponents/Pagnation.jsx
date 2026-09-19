d939b2f011b5c4cd3e62f3412acbd15c4a0b088a
rintymoni01 <rintymoni01@gmail.com>
2026-09-19 13:54:01 +0600

    page

--- src/Pages/Products/ProductsComponents/Pagnation.jsx
+++ src/Pages/Products/ProductsComponents/Pagnation.jsx
@@ -1,8 +1,8 @@
 
-const Pagnation = () => {
+const Pagnation = ({page , pageHandler}) => {
     return (
         <div className="mt-10 space-x-4">
-            <button className="">Prev</button>
+            <button className={`${page === 1 ? "bg-red-400" : "bg-red-500"}`}>Prev</button>
 
             <button>Next</button>
 
