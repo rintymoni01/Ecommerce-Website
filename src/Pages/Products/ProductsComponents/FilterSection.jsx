5b7be01e29daba30c17cd22497ac9412abeef564
rintymoni01 <rintymoni01@gmail.com>
2026-09-19 09:49:38 +0600

    onChange

--- src/Pages/Products/ProductsComponents/FilterSection.jsx
+++ src/Pages/Products/ProductsComponents/FilterSection.jsx
@@ -6,7 +6,9 @@ const FilterSection = ({search , setSearch , brand ,setBrand , priceRange, setPr
     <div className="bg-gray-100  mt-10 p-4 rounded-md h-max">
       <input
         type="text"
-        placeholder="Search..."    
+        placeholder="Search..." 
+        value={search}
+        onChange={(e)=> setSearch(e.target.value)}   
         className="bg-white p-2 rounded-md border-gray-400 border-2" />
 
 
@@ -17,7 +19,7 @@ const FilterSection = ({search , setSearch , brand ,setBrand , priceRange, setPr
       <div className="flex flex-col mt-3 gap-2 ">
         {categoryOnlyData?.slice(0, 7).map((item, index) => (
           <div key={index} className="flex gap-2">
-            <input type="checkbox" />
+            <input type="checkbox" name={item} checked ={category===item} value={item} onChange={} />
             <button className="cursor-pointer uppercase">{item}</button>
             {/* Apnar HTML content ekhane thakbe */}
           </div>
@@ -30,6 +32,7 @@ const FilterSection = ({search , setSearch , brand ,setBrand , priceRange, setPr
         <select
           name=""
           id=""
+          value={brand}
           className="bg-white w-full border-gray-200 border-2 rounded-md p-2 "
         >
           {brandOnlyData?.map((item, index) => {
