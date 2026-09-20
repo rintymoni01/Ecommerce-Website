8f3cc6726df10ddbf1d86fbcf67b24b8de6e2c56
rintymoni01 <rintymoni01@gmail.com>
2026-09-20 20:09:51 +0600

    h3 tag add

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,7 +2,42 @@
 const About = () => {
     return (
         <div>
-            about
+           <div>
+            <h3 className="text-xl font-semibold text-red-800">About Rinty</h3>
+            <p>Welcome to <span className="text-red-800 font-bold">SR</span>moni, your ultimate destination for authentic and premium cosmetics. From skin-loving skincare essentials to trendy makeup must-haves, we’re here to elevate your beauty routine with genuine products and exceptional service.</p>
+            <h3 className="text-xl font-semibold text-red-800">Our Mission</h3>
+            <p>At <span className="text-red-800 font-bold">SR</span>moni, our mission is to make authentic and high-quality beauty products accessible to everyone. We are passionate about helping you look and feel your best, boosting your confidence with safe, skin-friendly products — all at competitive prices and delivered with speed and care.</p>
+            <h3 className="text-xl font-semibold text-red-800">Why Choose</h3>
+           <div>
+            <div className=" flex gap-2 items-center">
+                 <div className="h-2 w-2 rounded-full bg-gray-700">  </div>
+                    <p className="text-sm">100% authentic and premium cosmetic products from trusted global and local brands</p>
+                
+            </div>
+            <div className=" flex gap-2 items-center">
+                <div></div>
+                <p>Lightning-fast and secure shipping</p>
+            </div>
+            <div className=" flex gap-2 items-center">
+                <div></div>
+                <p>Reliable customer support, always ready to help you choose the right product</p>
+            </div>
+            <div className=" flex gap-2 items-center">
+                <div></div>
+                <p>Easy returns and a hassle-free shopping experience</p>
+            </div>
+           </div>
+
+            <h3 className="text-xl font-semibold text-red-800">Our Vision</h3>
+            <p>We envision a future where self-care and beauty elevate everyday life. At <span className="text-red-800 font-bold">SR</span>moni, we’re committed to staying ahead of the curve, offering the best beauty solutions that are both effective and affordable.</p>
+
+            <h4 className="text-md font-semibold text-red-800"> Join the SRmoni Family</h4>
+            <p>Whether you're a makeup lover, a skincare enthusiast, or just looking for something fresh and glowing — <span className="text-red-800 font-bold">SR</span>moni has something special for everyone.</p>
+
+
+
+            <button className="px-4 py-1 bg-red-500 rounded-md text-white">Shop Now</button>
+           </div>
         </div>
     );
 };
