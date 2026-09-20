b7afc58ab1e78391a85ca125dc0b817908413fe2
rintymoni01 <rintymoni01@gmail.com>
2026-09-20 15:03:01 +0600

    logic

--- src/Pages/Products/ProductsComponents/Pagnation.jsx
+++ src/Pages/Products/ProductsComponents/Pagnation.jsx
@@ -1,4 +1,11 @@
 const Pagnation = ({ page, pageHandler ,dynamicPage }) => {
+    const getPages =(current, total)=>{
+const pages =[];
+if (total <= 5){
+    for(let i =1; i <= total; i++)
+        pages.push(i)
+}
+    }
   return (
     <div className="mt-10 space-x-4">
       <button
@@ -8,7 +15,7 @@ const Pagnation = ({ page, pageHandler ,dynamicPage }) => {
         Prev
       </button>
 
-      <button  disabled={page === 1}
+      <button  disabled={page === dynamicPage}
         className={`${page === dynamicPage ? "bg-red-400" : "bg-red-500"} text-white px-3 py-1 
         rounded-md cursor-pointer`}>Next</button>
     </div>
