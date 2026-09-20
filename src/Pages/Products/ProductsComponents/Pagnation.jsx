6f9d19211e2c7465fefaec96036331bfd61f777c
rintymoni01 <rintymoni01@gmail.com>
2026-09-20 15:30:24 +0600

    onclick

--- src/Pages/Products/ProductsComponents/Pagnation.jsx
+++ src/Pages/Products/ProductsComponents/Pagnation.jsx
@@ -13,24 +13,36 @@ if (total <= 5){
         pages.push(1,"...", current-1, current,current+1 , "...", total  )
     }
 }
+return pages;
     }
   return (
     <div className="mt-10 space-x-4">
       <button
         disabled={page === 1}
         className={`${page === 1 ? "bg-red-400" : "bg-red-500"} text-white px-3 py-1 
-        rounded-md cursor-pointer`} >
+        rounded-md cursor-pointer`} 
+        onClick={()=>pageHandler(page-1)}
+        >
         Prev
       </button>
       {
-        getPages(page ,dynamicPage).map((item, index)=>{
-            return
+        getPages(page ,dynamicPage)?.map((item, index)=>{
+            return(
+                <span key={index} 
+                onClick={()=> typeof item === "number" && pageHandler(item)}
+                className={`cursor-pointer ${item === page ? "font-bold text-red-600" : ""}`}>
+                    {item}
+                    </span>
+            )
         })
+        
       }
 
       <button  disabled={page === dynamicPage}
         className={`${page === dynamicPage ? "bg-red-400" : "bg-red-500"} text-white px-3 py-1 
-        rounded-md cursor-pointer`}>Next</button>
+        rounded-md cursor-pointer`}
+        onClick={()=>pageHandler(page-1)}
+        >Next</button>
     </div>
   );
 };
