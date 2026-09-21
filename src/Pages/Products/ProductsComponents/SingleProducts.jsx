aa46bba98828c852d14d5ec505b76ba7b8961851
rintymoni01 <rintymoni01@gmail.com>
2026-09-21 17:20:44 +0600

    api add

--- src/Pages/Products/ProductsComponents/SingleProducts.jsx
+++ src/Pages/Products/ProductsComponents/SingleProducts.jsx
@@ -1,14 +1,28 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 
-const SingleProducts = () => {
+const SingleProducts = () =>{ 
     const params = useParams()
-    console.log(params);
-    
+    console.log(params);}
+    const getsingleProduct = async ()=>{
+    try {
+        const res = await axios.get(`https://fakestoreapi.com/products/${params.id}`)
+        console.log(res);
+        
+    } catch (error) {
+        console.log(error);
+        
+        
+    };
+    useEffect(()=>{
+       getsingleProduct()
+    },[]);
     
     return (
         <div>
-            
+            hjjshs
         </div>
     );
 };
