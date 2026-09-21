4aff56acd4f780035b40bea975dd358170fec375
rintymoni01 <rintymoni01@gmail.com>
2026-09-21 21:09:39 +0600

    input button label add

--- src/Pages/Products/ProductsComponents/SingleProducts.jsx
+++ src/Pages/Products/ProductsComponents/SingleProducts.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Breadcrums from "./Breadcrums";
+import { IoCartOutline } from "react-icons/io5";
 
 const SingleProducts = () => {
   const { id } = useParams();
@@ -13,7 +14,6 @@ const SingleProducts = () => {
       const res = await axios.get(`https://dummyjson.com/products/${id}`);
 
       console.log(res.data);
-
       setSingleProduct(res.data);
     } catch (error) {
       console.log(error);
@@ -23,7 +23,6 @@ const SingleProducts = () => {
   useEffect(() => {
     getSingleProduct();
   }, [id]);
-  const OrginalPrice = math.round(singleProduct.price + (singleProduct.price * singleProduct.discount))
 
   return (
     <>
@@ -46,16 +45,44 @@ const SingleProducts = () => {
               <h1 className="md:text-3xl font-bold text-gray-800">
                 {singleProduct.title}
               </h1>
+
               <div className="text-gray-700">
                 {singleProduct.brand?.toUpperCase()}/{" "}
                 {singleProduct.category?.toUpperCase()}
               </div>
 
+              <h2 className="text-2xl text-red-500 font-bold mt-5">
+                ${singleProduct.price}{" "}
+                <span className="line-through text-gray-700">
+                  $
+                  {Math.round(
+                    singleProduct.price +
+                      (singleProduct.price * singleProduct.discountPercentage) /
+                        100,
+                  )}
+                </span>{" "}
+                <span className="bg-red-500 text-white px-4 py-2 rounded-full">
+                  {singleProduct.discountPercentage}% discount
+                </span>
+              </h2>
               <p className="text-gray-500 mt-3">{singleProduct.description}</p>
 
-              <h2 className="text-2xl text-red-500 font-bold mt-5">
-                ${singleProduct.price}
-              </h2>
+              <div className="flex  items-center gap-4">
+                <label htmlFor="" className="text-sm font-medium text-gray-700">
+                  Quantity:{" "} 
+                </label>
+                <input
+                  type="number"
+                  min={1}
+                  value={1}
+                  className="w-20 border border-gray-300 rounded-lg px-3 focus:ring-2 focus:ring-red-500  focus:outline-none p-1 "
+                />
+              </div>
+              <div className="flex gap-4 mt-4">
+                <button className="px-6 flex  bg-red-500 text-white rounded-md gap-2 py-2 text-lg">
+                  <IoCartOutline className="w-6 h-6"/> Add to Cart
+                </button>
+              </div>
             </div>
           </div>
         </div>
