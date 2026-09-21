966a907ab36edd5199c69c244edea159f70573a1
rintymoni01 <rintymoni01@gmail.com>
2026-09-21 17:35:12 +0600

    useState add

--- src/Pages/Products/ProductsComponents/SingleProducts.jsx
+++ src/Pages/Products/ProductsComponents/SingleProducts.jsx
@@ -1,30 +1,33 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+const SingleProducts = () => {
 
-const SingleProducts = () =>{ 
-    const params = useParams()
-    console.log(params);}
-    const getsingleProduct = async ()=>{
-    try {
-        const res = await axios.get(`https://fakestoreapi.com/products/${params.id}`)
-        console.log(res);
-        
-    } catch (error) {
-        console.log(error);
-        
-        
-    };
-    useEffect(()=>{
-       getsingleProduct()
-    },[]);
-    
-    return (
-        <div>
-            hjjshs
-        </div>
+  const params = useParams();
+  const [singleProduct , setSingleProduct] =useState('')
+  console.log(params);
+};
+const getsingleProduct = async () => {
+  try {
+    const res = await axios.get(
+      `https://fakestoreapi.com/products/${params.id}`,
     );
+    console.log(res);
+    const product = res.data.product;
+   setSingleProduct(product)
+console.log(product);
+
+
+
+  } catch (error) {
+    console.log(error);
+  }
+  useEffect(() => {
+    getsingleProduct();
+  }, []);
+
+  return <div>hjjshs</div>;
 };
 
 export default SingleProducts;
