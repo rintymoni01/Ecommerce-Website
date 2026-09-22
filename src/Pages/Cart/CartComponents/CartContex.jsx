fc6e097e63c6c68ce8231863035b15eb7484641e
rintymoni01 <rintymoni01@gmail.com>
2026-09-22 10:22:45 +0600

    children

--- src/Pages/Cart/CartComponents/CartContex.jsx
+++ src/Pages/Cart/CartComponents/CartContex.jsx
@@ -1,7 +1,10 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const CartContex = createContext (null)
 
 export const CartProvider = ({children})=> {
-
+const [cartItem , setCartItem] =useState([])
+return <CartContex.Provider value={'cartItem , setCartItem'}>
+    {children}
+</CartContex.Provider>
 }
