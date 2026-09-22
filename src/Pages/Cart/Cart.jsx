461e07c084550804a669668fc9dbd36de5c09eeb
rintymoni01 <rintymoni01@gmail.com>
2026-09-22 13:27:40 +0600

    title price add

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,31 +1,46 @@
 import { useCart } from "../../Contex/CardContex";
 
-
-
 const Cart = () => {
-    const {cartItem} = useCart()
-    return (
-        <div className="mt-10 max-w-6xl mx-auto mb-5">
-      {
-        cartItem.length > 0 ? <div>
-            <h1 className="font-bold text-2xl ">My Cart ({cartItem.length})</h1>
-            <div>
-                <div className="mt-10">
-                    {
-                        cartItem.map((item, index)=>{
-                            return <div key={index}>
-                                <div>
-                                    
-                                </div>
-                            </div>
-                        })
-                    }
-                </div>
+  const { cartItem } = useCart();
+  return (
+    <div className="mt-10 max-w-6xl mx-auto mb-5">
+      {cartItem.length > 0 ? (
+        <div>
+          <h1 className="font-bold text-2xl ">My Cart ({cartItem.length})</h1>
+          <div>
+            <div className="mt-10">
+              {cartItem.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="bg-gray-100 p-5 rounded-md flex items-center justify-between  mt-3 w-full"
+                  >
+                    <div className="flex items-center gap-4">
+                      <img
+                        src={item.images}
+                        alt={item.title}
+                        className="w-20 h-20 rounded-md"
+                      />
+                      <div>
+                        <h1 className=" w-[300px] line-clamp-2">
+                          {item.title}
+                        </h1>
+                        <p className="text-red-500 font-semibold text-lg">
+                          ${item.price}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
-        </div>: <div>cart is empty</div>
-      }
+          </div>
         </div>
-    );
+      ) : (
+        <div>cart is empty</div>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
