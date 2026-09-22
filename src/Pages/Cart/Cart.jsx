1ecec6e7552c4da898ecee93363c9aa8720db47f
rintymoni01 <rintymoni01@gmail.com>
2026-09-22 13:15:06 +0600

    h1 tag add

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,10 +1,19 @@
+import { useCart } from "../../Contex/CardContex";
 
 
 
 const Cart = () => {
+    const {cartItem} = useCart()
     return (
-        <div>
-       wqe3wer
+        <div className="mt-10 max-w-6xl mx-auto mb-5">
+      {
+        cartItem.length > 0 ? <div>
+            <h1 className="font-bold text-2xl ">My Cart ({cartItem.length})</h1>
+            <div>
+                <div className="mt-10"></div>
+            </div>
+        </div>: <div>cart is empty</div>
+      }
         </div>
     );
 };
