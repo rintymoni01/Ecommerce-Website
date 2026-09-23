b20f373354e6a4b51f12f3e7e9f6bb817252f9c0
rintymoni01 <rintymoni01@gmail.com>
2026-09-23 19:20:46 +0600

    fakeApi

--- src/Pages/CategoryProduct.jsx
+++ src/Pages/CategoryProduct.jsx
@@ -1,5 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const CategoryProduct = () => {
+    const [searchData, setSearchData] = useState([])
+    const params =useParams()
+    const category =params.category
+    const getFilterData = async ()=>{
+        try {
+           const res = await axios.get(`https://fakestoreapi.in/api/product/category?type=${category}`)
+           const data =res.data.products 
+           setSearchData(data)
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+    console.log(category);
+    useEffect
+    
     return (
         <div>
             fsdese
