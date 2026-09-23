19d6d0bd9c19c5178e16c5d34c96c7daf35e25fc
rintymoni01 <rintymoni01@gmail.com>
2026-09-23 20:03:54 +0600

    image add

--- src/Components/ProductListView.jsx
+++ src/Components/ProductListView.jsx
@@ -1,9 +1,11 @@
 
 
-const ProductListView = () => {
+const ProductListView = ({product}) => {
     return (
         <div className="space-y-4 mt-2 rounded-md">
-            
+            <div className="bg-gray-100 gap-7 flex items-center rounded-md p-2 ">
+<img src={product.images} alt="" />
+            </div>
         </div>
     );
 };
