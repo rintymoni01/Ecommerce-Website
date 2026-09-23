bc22ec1404243bee2bfcf6bb1e85b2531fb58ebb
rintymoni01 <rintymoni01@gmail.com>
2026-09-23 20:27:28 +0600

    onclick

--- src/Components/ProductListView.jsx
+++ src/Components/ProductListView.jsx
@@ -1,13 +1,23 @@
+import { useNavigate } from "react-router-dom";
 
+const ProductListView = ({ product }) => {
+  const navigate = useNavigate();
+  return (
+    <div className="space-y-4 mt-2 rounded-md">
+      <div className="bg-gray-100 gap-7 flex items-center rounded-md p-2 ">
+        <img
+          src={product.images}
+          alt={product.title}
+          className="h-60  w-60 rounded-md  cursor-pointer"
+          onClick={() => navigate(`/product/${product.id}`)}
+        />
+        <div className="space-y-2 ">
+            <h1>Free Delivery Fri, is Apr Or Fastest Delivery Tomorrow, 17 Apr</h1>
 
-const ProductListView = ({product}) => {
-    return (
-        <div className="space-y-4 mt-2 rounded-md">
-            <div className="bg-gray-100 gap-7 flex items-center rounded-md p-2 ">
-<img src={product.images} alt="" />
-            </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ProductListView;
