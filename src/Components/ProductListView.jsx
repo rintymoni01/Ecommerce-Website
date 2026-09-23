e039b79bea120d5d99e503c804cdf67e9fedb295
rintymoni01 <rintymoni01@gmail.com>
2026-09-23 20:46:59 +0600

    img

--- src/Components/ProductListView.jsx
+++ src/Components/ProductListView.jsx
@@ -6,17 +6,18 @@ const ProductListView = ({ product }) => {
     <div className="space-y-4 mt-2 rounded-md">
       <div className="bg-gray-100 gap-7 flex items-center rounded-md p-2 ">
         <img
-          src={product.images}
+          src={product.images?.[0]}
           alt={product.title}
-          className="h-60  w-60 rounded-md  cursor-pointer"
+          className="h-60 w-60 rounded-md cursor-pointer object-cover"
           onClick={() => navigate(`/product/${product.id}`)}
         />
         <div className="space-y-2 ">
-          
-         <p className=" font-bold text-xl hover:text-red-400 w-full pr-30 ">{product.description}</p>
+          <p className=" font-bold text-xl hover:text-red-400 w-full pr-30 ">
+            {product.description}
+          </p>
           <p className="font-semibold flex items-center text-lg ">
-            <span className="text-4xl">{product.price}</span>({product.discount}% off
-            )
+            <span className="text-4xl">{product.price}</span>({product.discount}
+            % off )
           </p>
           <p>
             Free Delivery Fri, is Apr <br /> Or Fastest Delivery Tomorrow, 17
