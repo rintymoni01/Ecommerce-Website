1eeb6d5f531b97b19f1495727f20f9fb04c6156f
rintymoni01 <rintymoni01@gmail.com>
2026-09-23 22:22:34 +0600

    css add

--- src/Components/ResponcivedMenu.jsx
+++ src/Components/ResponcivedMenu.jsx
@@ -1,10 +1,9 @@
-
-const ResponcivedMenu = ({openNav, setOpenNav}) => {
-    return (
-        <div className={(openNav ? "left-0":"-left-[100%]")}>
-            
-        </div>
-    );
+const ResponcivedMenu = ({ openNav, setOpenNav }) => {
+  return (
+    <div
+      className={`(openNav ? "left-0":"-left-[100%]") fixed top-0 bottom-0 z-20 flex h-screen w-[75%]  flex-col justify-between  bg-white px-8 pb-6 pt-16 rounded-r-xl text-black  shadow-md transition-all `}
+    ></div>
+  );
 };
 
 export default ResponcivedMenu;
