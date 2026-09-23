3552bdce7c76f54daf5400b3a2f2dfea9dde3dad
rintymoni01 <rintymoni01@gmail.com>
2026-09-23 22:24:58 +0600

    css add

--- src/Components/ResponcivedMenu.jsx
+++ src/Components/ResponcivedMenu.jsx
@@ -2,7 +2,13 @@ const ResponcivedMenu = ({ openNav, setOpenNav }) => {
   return (
     <div
       className={`(openNav ? "left-0":"-left-[100%]") fixed top-0 bottom-0 z-20 flex h-screen w-[75%]  flex-col justify-between  bg-white px-8 pb-6 pt-16 rounded-r-xl text-black  shadow-md transition-all `}
-    ></div>
+    >
+        <div>
+            <div className=" flex items-center justify-start gap-3">
+
+            </div>
+        </div>
+    </div>
   );
 };
 
